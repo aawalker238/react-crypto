682e7514d36c77d9ff7005fd1d5c048c78d9a42f
Anthony Walker <anthonyaustinwalker@gmail.com>
2019-01-04 12:04:53 -0800

    adding setting spage and localStorage

---
+++ src/Settings/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage';
+import ConfirmButton from './ConfirmButton';
+
+const index = () => {
+  return (
+    <React.Fragment>
+      <WelcomeMessage />
+      <ConfirmButton />
+    </React.Fragment>
+  );
+};
+
+export default index;
